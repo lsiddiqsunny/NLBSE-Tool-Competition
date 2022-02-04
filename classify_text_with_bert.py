219361c9ec1d2275f17470a652bb2a5f1863757e
lsiddiqsunny <msiddiq3@nd.edu>
2022-02-03 21:46:10 -0500

    Updated

--- classify_text_with_bert.py
+++ classify_text_with_bert.py
@@ -5,7 +5,6 @@ import shutil
 
 import tensorflow as tf
 import tensorflow_hub as hub
-import tensorflow_text as text
 from official.nlp import optimization  # to create AdamW optimizer
 
 import matplotlib.pyplot as plt
